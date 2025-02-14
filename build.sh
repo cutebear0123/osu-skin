49fc33af642ae108cf69645aa41fa0b8d7a19348
cutebear0123 <cutebear@0w0b.dev>
2025-02-13 17:08:02 -0800

    fix triangle

--- build.sh
+++ build.sh
@@ -12,5 +12,5 @@ git add .
 read -p "Enter Commit Message: " commit
 git commit -m "$commit"
 git push
-zip -r "Mahiru V$(cat version.txt) ($(git rev-parse --short HEAD))".osk build
+zip -r "V$(cat version.txt) ($(git rev-parse --short HEAD))".osk build
 git rev-parse --short HEAD
